6916e884e340c199bfbe50eb44649357c6d6a258
John Cheng <johnlicheng@gmail.com>
2025-04-06 13:34:40 -0700

    Adds demonstration on the effects of the official solution

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 
 from jcft.main_app import app
 
 
 def main():
+    wrapped_app = CORSMiddleware(app, allow_origins=["*"])  # https://github.com/fastapi/fastapi/discussions/8027
     uvicorn.run(
-        app,
+        wrapped_app,
         host="0.0.0.0",
         port=8080,
     )

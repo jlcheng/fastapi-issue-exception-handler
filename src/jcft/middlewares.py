f1103ec24b47b094373168d515956705c0236eb1
John Cheng <johnlicheng@gmail.com>
2025-04-06 14:40:14 -0700

    Improved solution for the authenticated URL use case

--- src/jcft/middlewares.py
+++ src/jcft/middlewares.py
@@ -3,12 +3,12 @@ from starlette.middleware.cors import CORSMiddleware
 
 
 def add_cors_middleware(app: FastAPI):
+    # See https://github.com/fastapi/fastapi/discussions/8027
+    # The parameters here must be in sync with the parameters used to
+    # launch the FastAPI instance. In this case it must
+    # match src/main.py.
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["https://example.com"],
         allow_credentials=True,
-        allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-        allow_headers=["*"],
-        expose_headers=[],
-        max_age=600,
     )
--- src/main.py
+++ src/main.py
@@ -5,7 +5,18 @@ from jcft.main_app import app
 
 
 def main():
-    wrapped_app = CORSMiddleware(app, allow_origins=["*"])  # https://github.com/fastapi/fastapi/discussions/8027
+    # See https://github.com/fastapi/fastapi/discussions/8027
+    # This expands on that solution to consider the case where the affected
+    # URL requires cookie-based authentication.
+    #
+    # The parameters here must match the parameters used by the
+    # CORSMiddleware instance used in `app.add_middleware()`. In this case,
+    # it must match the code in `middlewares.py`.
+    wrapped_app = CORSMiddleware(
+        app,
+        allow_origins=["https://example.com"],
+        allow_credentials=True,
+    )
     uvicorn.run(
         wrapped_app,
         host="0.0.0.0",
